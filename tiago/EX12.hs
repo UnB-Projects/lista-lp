f0b3e1a66111d846cb360b3f2e47ae74b0330c5c
Tiago Cabral <tiagocabral@cjr.org.br>
2017-09-16 23:52:34 -0300

    Agora sim o EX12, e.e

--- tiago/EX12.hs
+++ tiago/EX12.hs
@@ -4,6 +4,7 @@ maiuscula x | (x>='A' && x<='Z') = True
 procura_resto [] = []
 procura_resto (x:xs) | (x>='a' && x<='z') = x:procura_resto xs
                      | maiuscula x = procura_nome (x:xs)
+                     | (x == ' ') = procura_nome (x:xs) 
                      | otherwise = procura_resto xs
 
 procura_nome [] = []
